18552b593799296abee0b16eea81a216cae3a7a0
pellegoeg <pellegoeg@users.noreply.github.com>
2022-02-16 13:37:33 +0100

    updated itsplatform

--- itsplatform/datavask.py
+++ itsplatform/datavask.py
@@ -2,19 +2,17 @@
 
 Vi skal importere filen bil_import_2022.csv til en pandas dataframe.
 
-Der skal dannes et overblik over dataene identificier problemer med fdaa
-    Få et overblik over dine data, lav forskellige SELECT forespørgsler (queries)
-        Bør datatyperne være anderledes end varchar?
-        Hvordan er felterne/kollonernes indhold?
-    Lav en ny tabel, bil, på baggrund af public.bil_import men hvor datatyperne er sat korrekt.
-        Der mangler en bil, indsæt denne i public.bil:
-
-        Id: 1923, Manufactor: Kia, Model: Yaris, Drivmiddel: El, KmPrAar: 7500, Årgang: 2016
-
-        Hvor mange forskellige bilmærker har vi før og efter vask?
-    Hvilke biler har kørt mindst 20.000 km i kører på benzin (type=2)?
-        Hvor mange biler er det?
-        Hvor langt har de kørt ialt?
-    Hvad er 10, 25 og 75 % fraktilen for kørte km.
-        Hvordan ser det ud indenfor de enkelte bilmærker
+1. Der skal dannes et overblik over dataene. Hvad er indholdet, hvordan er datatyperne?
+2. Få et overblik over dine data, lav forskellige forespørgsler på dataframen
+   - Bør datatyperne være anderledes end varchar?
+   - Hvordan er felterne/kollonernes indhold?
+3. Lav en ny tabel, bil, på baggrund af public.bil_import men hvor datatyperne er sat korrekt.
+   - Der mangler en bil, indsæt denne i public.bil:
+     Id: 1923, Manufactor: Kia, Model: Yaris, Drivmiddel: El, KmPrAar: 7500, Årgang: 2016
+   - Hvor mange forskellige bilmærker har vi før og efter vask?
+4. Hvilke biler har kørt mindst 20.000 km i kører på benzin (type=2)?
+    - Hvor mange biler er det?
+    - Hvor langt har de kørt ialt?
+5. Hvad er 10, 25 og 75 % fraktilen for kørte km.
+    - Hvordan ser det ud indenfor de enkelte bilmærker
 '''
