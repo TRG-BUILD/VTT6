721e529c35245ee743989a5a03b3e6bf48ebcd75
pellegoeg <pellegoeg@users.noreply.github.com>
2022-03-07 13:08:00 +0100

    Updated alpr_analysis.py

--- nummerplade/alpr_analysis.py
+++ nummerplade/alpr_analysis.py
@@ -1,15 +1,16 @@
 # coding=utf-8
 import pladelib
 import argparse
+import psycopg2
 
 
-def clean_input(file):
+def clean_input(file) -> list[list[Any]]:
     """
     Open input file and remove empty frames and parse frames with input for returning af list of list
     inputs:
         file - Input file, coming from openALPR
     output:
-        cleaned_input - List of plates, with frame nummer aka a list(list(frame, plate)) 
+        cleaned_input - List of plates, with frame nummer aka a list(list(frame, plate))
     """
 
     cleaned_input = []
@@ -17,7 +18,7 @@ def clean_input(file):
     return cleaned_input
 
 
-def clean_plates(cleaned_input):
+def clean_plates(cleaned_input: list[list[Any]]) -> list[list[Any]]:
     """
     Check each result in cleaned_input, to see if the plate is passing our validation, Here you should use your pladelib.py library
 
@@ -29,7 +30,7 @@ def clean_plates(cleaned_input):
     return validated
 
 
-def get_seen_plates(validated, least_seen=4):
+def get_seen_plates(validated, least_seen=4) -> list[dict[str, Any]]:
     """
     Count how many times a plate have been seen, and return plates seen enough times to accept the result
 
@@ -37,30 +38,50 @@ def get_seen_plates(validated, least_seen=4):
         validated -  List of plates, with frame nummer aka a list(list(frame, plate))
         least_seen - How many times should we have seen the plates before we accept it
     output:
-        seen_plates - Dictionary idea for scructure: seen_plates['AA12345'] = { 'count': 22, 'first_seen': 22, 'last_seen': 25}
+        seen_plates - List of dictionaries idea with the structure: { 'plate': "AA12345" ,'count': 22, 'first_seen': 22, 'last_seen': 25}
     """
 
 
-def save_to_csv(output):
+def save_to_csv(counted_plates: list[dict[str, Any]]) -> None:
     """
     Save result of seen licenseplates to file
 
     inputs:
-        seen_plates - Dictonary of plates seen, and when it have been seen
+        seen_plates - List of dictonaries of plates seen, and when it have been seen
     output:
         None (file as csv)
     """
 
-def save_to_db(output):
+
+def save_to_db(counted_plates: list[dict[str, Any]]) -> None:
     """
-    Create connection to database, save licenseplate and framenumber.
+    Create connection to database, save licenseplate including the information we know.
+
+    Table in database:
+    drop table if exists alpr_clean;
+    create table alpr_clean (
+        plate varchar(7),
+        count int,
+        first_seen int,
+        last_seen int
+    );
 
     inputs:
-        seen_plates - Dictonary of plates seen, and when it have been seen
+        seen_plates - list of dictionary with plate as key, and dictionary of count, first_seen, last_seen
     output:
-        None (INSERT query to postgresql)
+        None
     """
+    conn = psycopg2.connect(
+        dbname="vttt", host="localhost", user="postgres", password="changeme"
+    )
+    cursor = conn.cursor()
 
+    # Insert statement
+    for item in counted_plates:
+        query = f""
+        cursor.execute(query)
+    else:
+        conn.commit()
 
 
 if __name__ == "__main__":
